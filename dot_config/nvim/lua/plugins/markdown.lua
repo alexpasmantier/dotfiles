0cae46c753a44a898f93e1ad39fc2fd062314be2
Alex Pasmantier <alex.pasmant@gmail.com>
2025-11-14 10:59:16 +0100

    hyprland and nvim tweaks

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,11 +1,11 @@
 return {
   -- {
-  --   "MeanderingProgrammer/markdown.nvim",
-  --   name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
-  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
-  --   config = function()
-  --     require("render-markdown").setup({})
-  --   end,
+  --   "MeanderingProgrammer/render-markdown.nvim",
+  --   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  --   ---@module 'render-markdown'
+  --   opts = {},
   -- },
   {
     "iamcco/markdown-preview.nvim",
